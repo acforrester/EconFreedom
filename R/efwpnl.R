f6687e58261aa2422594499025086eb4063e51f0
acforrester <ac.forrester@protonmail.ch>
2021-10-15 19:02:01 -0400

    UPDT: Major Update
    - Update to 2021 EFW
    - Update all documentation
    - Update EFW dataset (includes World Bank regions)
    - Add demonstrations on pkg site

--- R/efwpnl.R
+++ R/efwpnl.R
@@ -4,9 +4,12 @@
 #'
 #' @format A data frame:
 #' \describe{
-#'   \item{iso_code}{Country ISO Alpha-3 code}
+#'   \item{iso_code}{Country ISO Alpha-3 Node}
+#'   \item{country_name}{Country Name}
 #'   \item{year}{Year}
-#'   \item{country_name}{Country name}
+#'   \item{region_code}{World Bank Region (ISO Alpha-3)}
+#'   \item{region_name}{World Bank Region}
+#'   \item{income_group}{World Bank Income Group}
 #'   \item{}{Area 1 - Size of Government}
 #'   \item{}{Area 2 - Legal System and Property Rights}
 #'   \item{}{Area 3 - Sound Money}
--- data-prep/prep_data.R
+++ data-prep/prep_data.R
@@ -14,16 +14,61 @@
 if (!require("pacman")) install.packages("pacman")
 pacman::p_load(tidyverse)
 
-# EFW Panel Dataset -------------------------------------------------------
+# -------------------------------------------------------------------------
+# Metadata from the World Bank:
+#
+# In this step I add in World Bank region and income group classifiers to
+# join on the EFW data in place of the provided regions. This modification
+# adds the World Bank's region code and the income group
+# -------------------------------------------------------------------------
+
+
+# World Bank regions
+url = "http://databank.worldbank.org/data/download/site-content/CLASS.xlsx"
+file = paste0("data-prep/", basename(url))
+
+# download file
+if (!file.exists(file)) {
+  download.file(url, file, mode = "wb")
+}
+
+# clean up countries
+wb_group <- readxl::read_excel(file) %>%
+  rename(wb_name      = 1,
+         iso_code     = 2,
+         region_name  = 3,
+         income_group = 4) %>%
+  select(1:4)
+
+# clean up regions
+wb_codes <- wb_group %>%
+  filter(!is.na(wb_name), is.na(region_name)) %>%
+  select(region_name = wb_name, region_code = iso_code)
+
+# now combine the data
+wb_group <- wb_group %>%
+  filter(!is.na(region_name)) %>%
+  left_join(., wb_codes, by = "region_name") %>%
+  # reorder
+  select(iso_code, wb_name, region_code, region_name, income_group)
+
+# -------------------------------------------------------------------------
+# Economic Freedom of the World (EFW)
+#
+# Download and clean up the Gwartney et al. Economic Freedom of the World
+# (EFW) data.
+# -------------------------------------------------------------------------
 
 # EFW data
 url  = "https://www.fraserinstitute.org/sites/default/files/economic-freedom-of-the-world-2021-master-index-data-for-researchers-iso.xlsx"
 file = paste0("data-prep/", basename(url))
 
+# download file
 if (!file.exists(file)) {
   download.file(url, file, mode = "wb")
 }
 
+# now clean up the EFW data
 efwpnl <- readxl::read_excel(file, sheet = 2) %>%
   rename(
     year          = `Year`,
@@ -36,13 +81,23 @@ efwpnl <- readxl::read_excel(file, sheet = 2) %>%
     trade_freedom = `Area 4`,
     regulation    = `Area 5`
   ) %>%
-  select(iso_code, year, country_name, everything()) %>%
+  # join on the World Bank metadata
+  left_join(., wb_group, by = "iso_code") %>%
+  # order columns
+  select(iso_code, country_name, year, region_code, region_name, income_group,
+         everything(), -wb_name) %>%
   arrange(iso_code, year)
 
 # save internal data
 usethis::use_data(efwpnl, overwrite = T)
 
-# EFNA Panel Dataset ------------------------------------------------------
+# -------------------------------------------------------------------------
+# Economic Freedom of North America (EFNA)
+#
+# Download and clean up the Stansel et al. Economic Freedom of North
+# America (EFNA) data. Also add in the U.S. state FIPS codes for easy
+# use with other datasets.
+# -------------------------------------------------------------------------
 
 # state FIPS codes
 stfips <- read_delim("https://www2.census.gov/geo/docs/reference/state.txt", delim = "|") %>%
@@ -54,10 +109,12 @@ stfips <- read_delim("https://www2.census.gov/geo/docs/reference/state.txt", del
 url  = "https://www.fraserinstitute.org/sites/default/files/economic-freedom-north-america-2020-panel-dataset.xlsx"
 file = paste0("data-prep/", basename(url))
 
+# download file
 if (!file.exists(file)) {
   download.file(url, file, mode = "wb")
 }
 
+# now clean up the EFNA data
 efna <- readxl::read_excel(file, sheet = 1) %>%
   rename(state_name = `Subnational Index`,
          year = `...2`,
@@ -70,4 +127,4 @@ efna <- readxl::read_excel(file, sheet = 1) %>%
   arrange(stfips, year)
 
 # save internal data
-usethis::use_data(efna)
+usethis::use_data(efna, overwrite = T)
